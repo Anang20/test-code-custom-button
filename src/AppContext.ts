622a7400da5b7b1f8a63561423f0dc7a331d4143
anangsyah <anang.haqim@ist.id>
2024-05-05 13:07:04 +0700

    fix: linter

--- src/AppContext.ts
+++ src/AppContext.ts
@@ -3,20 +3,28 @@ import { IconPositionType, SizeType, TooltipPositionType } from '@/types';
 
 const AppContext = createContext({
     label: "",
+    // @ts-ignore
     handleChangeLabel: (event: React.ChangeEvent<HTMLInputElement>) => {},
     background: "",
+    // @ts-ignore
     handleChangeBackground: (val: string) => {},
     size: "",
+    // @ts-ignore
     handleChangeSize: (val: SizeType) => { },
     isTooltip: false,
+    // @ts-ignore
     handleChangeIsTooltip: (val: boolean) => {},
     tooltip: "",
+    // @ts-ignore
     handleChangeTooltip: (event: React.ChangeEvent<HTMLInputElement>) => { },
     tooltipPosition: "",
+    // @ts-ignore
     handleChangeTooltipPosition: (val: TooltipPositionType) => { },
     icon: "",
+    // @ts-ignore
     handleChangeIcon: (val: string) => { },
     iconPosition: "",
+    // @ts-ignore
     handleChangeIconPosition: (val: IconPositionType) => {}
 });
 
--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -16,7 +16,6 @@ import {
     Loader2,
     LogIn,
     LucideIcon,
-    LucideProps,
     MenuIcon,
     Moon,
     MoreVertical,
